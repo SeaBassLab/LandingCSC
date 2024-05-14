5f1ad661d4414d73b6f7948fd239f71a93fb9a05
SeaBassLab <SeaBassLab>
2024-05-14 19:20:36 +0000

    Pushed from Locofy Builder

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,7 +5,7 @@ import DownloadBookSection from "./DownloadBookSection";
 
 const Main: FunctionComponent = () => {
   return (
-    <div className="w-[1280px] bg-primary-25 overflow-hidden flex flex-col items-center justify-start py-8 px-28 box-border gap-[40px]">
+    <div className="self-stretch bg-primary-25 overflow-hidden flex flex-col items-center justify-start py-8 px-28 gap-[40px]">
       <Text className="w-[300px] relative" variant="h1" />
       <Proyects />
       <DownloadBookSection />
