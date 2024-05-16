d5bb41586bd5e86d30f5ab38a390603c25d955b5
SeaBassLab <SeaBassLab>
2024-05-16 14:59:04 +0000

    Pushed from Locofy Builder

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,12 +6,12 @@ const Hero: FunctionComponent = () => {
   return (
     <div className="bg-base-white overflow-hidden flex flex-col items-center justify-start pt-16 px-28 pb-8 relative gap-[120px]">
       <img
-        className="w-[25.94%] absolute !m-[0] h-[55.21%] top-[-17.03%] right-[-7.55%] bottom-[61.82%] left-[81.62%] max-w-full overflow-hidden max-h-full object-contain z-[3]"
+        className="w-[31.45%] absolute !m-[0] h-[67.73%] top-[-19.4%] right-[-15.72%] bottom-[51.67%] left-[84.27%] max-w-full overflow-hidden max-h-full object-contain z-[3]"
         alt=""
         src="/group-2697922@2x.png"
       />
       <img
-        className="w-[11.47%] absolute !m-[0] h-[31.74%] top-[58.93%] right-[91.19%] bottom-[9.33%] left-[-2.67%] max-w-full overflow-hidden max-h-full object-contain z-[2]"
+        className="w-[11.57%] absolute !m-[0] h-[31.77%] top-[58.91%] right-[91.29%] bottom-[9.32%] left-[-2.87%] max-w-full overflow-hidden max-h-full object-contain z-[2]"
         alt=""
         src="/group-2697932@2x.png"
       />
